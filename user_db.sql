fafba8e1c72bdea8891db879db1f901e89be27e6
hoang26112004 <hoang261104@gmail.com>
2025-06-18 22:37:40 +0700

    Add files via upload

---
+++ user_db.sql
@@ -0,0 +1,28 @@
+
+DROP TABLE ACCOUNT;
+DROP TABLE USERS;
+
+-- Tạo bảng USER
+CREATE TABLE USERS (
+    UserId     INT PRIMARY KEY,
+    Email      VARCHAR2(50) NOT NULL,
+    Name       VARCHAR2(50) NOT NULL,
+    Phone      VARCHAR2(20)
+);
+
+-- Tạo bảng ACCOUNT
+CREATE TABLE ACCOUNT (
+    Username    VARCHAR2(50) PRIMARY KEY,
+    System      VARCHAR2(50) NOT NULL,
+    Create_At   DATE DEFAULT SYSDATE,
+    Update_At   DATE DEFAULT SYSDATE,
+    Status      VARCHAR2(20) DEFAULT 'ACTIVE' CHECK (Status IN ('ACTIVE', 'DISABLED', 'LOCKED')),
+    UserId      INT NOT NULL,
+    CONSTRAINT fk_account_user FOREIGN KEY (UserId)
+        REFERENCES USERS(UserId)
+        ON DELETE CASCADE
+);
+
+-- Lấy toàn bộ dữ liệu
+SELECT * FROM USERS;
+SELECT * FROM ACCOUNT;
