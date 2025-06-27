85e832e7f04dcc26224860b0885aa816240646d4
hoang26112004 <hoang261104@gmail.com>
2025-06-27 10:23:43 +0700

    Delete AccountRepository.java

--- AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.accountSystem.repository;
-import com.example.accountSystem.Entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account,String> {
-
-}
