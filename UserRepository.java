faff3624ac1920cd2e6490eaba2546a14d05b009
hoang26112004 <hoang261104@gmail.com>
2025-06-27 10:24:42 +0700

    Delete UserRepository.java

--- UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.accountSystem.repository;
-import com.example.accountSystem.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-}
